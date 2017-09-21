aecd3a055a5de5dfb239f778cdfb586e193ab85f
mikeysax <michaelmerrittfarrell@gmail.com>
2017-09-21 12:22:29 -0400

    Created seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Note.create(title: 'Welcome!', content: 'This note was auto-generated for you.')
+Note.create(title: 'React', content: 'In these lessons, you will learn all about React. Enjoy! :)')
