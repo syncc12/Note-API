172b9c222535dee0a2c022cbfa460cd94b747282
mikeysax <michaelmerrittfarrell@gmail.com>
2017-11-27 14:28:28 -0500

    Fixed routes and tags controller.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -10,6 +10,7 @@ class TagsController < ApplicationController
   def destroy
     tag = Tag.find(params[:id])
     tag.destroy
+    head :no_content
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :notes do
-    resources :tags, only: [:create, :destroy]
+    resources :tags, only: [:create]
   end
+  resources :tags, only: [:destroy]
 end
